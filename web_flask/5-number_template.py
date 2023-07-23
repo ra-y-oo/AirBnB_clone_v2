43a602eea66279d982373fb644b7a68afe5cf8b9
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-23 06:39:27 +0300

    Add templates directory

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    formatted = text.replace('_', ' ')
+    return f"C {formatted}"
+
+@app.route("/pythons/(<text>)", strict_slashes=False)
+def python(text):
+    text = "is cool"
+    formatted = text.replace('_','')
+    return f"Python {formatted}"
+
+@app.route("/number/<n>", strict_slashes=False)
+def check_int(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def check_int(n):
+    if isinstance(n, int):
+            return render_template("5-number.html", number=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
