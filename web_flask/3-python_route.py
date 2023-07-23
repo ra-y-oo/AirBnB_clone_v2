df3abf1f6dc077c27481d6c1d013d25a0b5f8544
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-23 06:24:35 +0300

    Create 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    formatted = text.replace('_', ' ')
+    return f"C {formatted}"
+
+@app.route("/pythons/(<text>)", strict_slashes=False)
+def python(text):
+    text = "is cool"
+    formatted = text.replace('_','')
+    return f"Python {formatted}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
