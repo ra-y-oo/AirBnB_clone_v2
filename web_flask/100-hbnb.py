8e37c39c37e1d93e05bbde07eb1df4e4e5af305f
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-24 02:56:11 +0300

    Add files

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from models import storage, State, Amenity
+
+app = Flask(__name__)
+
+@app.route('/hbnb', methods=['GET'])
+def hbnb():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    amenities = storage.all(Amenity).values()
+    sorted_amenities = sorted(amenities, key=lambda amenity: amenity.name)
+
+    return render_template('100-hbnb.html', states=sorted_states, amenities=sorted_amenities)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
