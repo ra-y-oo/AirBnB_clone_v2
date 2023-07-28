fdab468f0c8bec8d299961430ae0d29d719400ad
Rayoo <105439282+ra-y-oo@users.noreply.github.com>
2023-07-28 04:24:01 +0300

    Update file

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,23 +1,27 @@
-from flask import Flask, render_template
-from models import storage, State, City
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
-@app.route('/hbnb-filters', strict_slashes=False)
-def hbnb_filters():
-    # Fetch and sort State, City, and Amenity objects by name
-    states = sorted(list(storage.all('State').values()), key=lambda x: x.name)
-    cities = sorted(list(storage.all('City').values()), key=lambda x: x.name)
-    amenities = sorted(list(storage.all('Amenity').values()), key=lambda x: x.name)
 
-    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities)
-
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """display a HTML page like 6-index.html from static"""
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,32 +1,28 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
-from models import storage, State, City
-
+from models import *
+from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/states', methods=['GET'], strict_slashes=False)
-def states():
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
-    return render_template('states.html', data=sorted_states)
-
-
-@app.route('/states/<id>', methods=['GET'], strict_slashes=False)
-def state_cities(id):
-    state = storage.get(State, id)
-    if state is not None:
-        cities = state.cities if state.cities else state.cities()
-        sorted_cities = sorted(cities, key=lambda city: city.name)
-        return render_template('states.html', data=sorted_cities, state_name=state.name, is_state=True)
-    else:
-        return render_template('states.html', state_name='Not found!', is_state=False)
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
